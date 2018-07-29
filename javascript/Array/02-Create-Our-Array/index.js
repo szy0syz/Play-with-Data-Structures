0f8f8ab0307ff039bf2bba4a7f4798f1b5d1a06b
szy0syz <szy0syz@gmail.com>
2018-07-29 10:55:06 +0800

    update 2-3 向数组中添加元素 && 重构类的私有属性

--- javascript/Array/02-Create-Our-Array/index.js
+++ javascript/Array/02-Create-Our-Array/index.js
@@ -1,23 +1,27 @@
-class DArray {
-  // private int[] _data;
-  // private int _size;
+const DArray = (function () {
+  let _data = Symbol('data')
+  let _size = Symbol('size')
 
-  constructor(capacity = 10) {
-    this._data = new Array(capacity)
-    this._size = 0
+  class DArray {
+    constructor(capacity = 10) {
+      this[_data] = new Array(capacity)
+      this[_size] = 0
+    }
+
+    getCapacity() {
+      return this[_data].length
+    }
+
+    getSize() {
+      return this[_size]
+    }
+
+    isEmpty() {
+      return this[_size] === 0
+    }
   }
 
-  getCapacity() {
-    return this._data.length
-  }
-
-  getSize() {
-    return this._size
-  }
-
-  isEmpty() {
-    return this._size === 0
-  }
-}
+  return DArray
+})()
 
 export default DArray
--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,13 @@
-import DArray from './Array/02-Create-Our-Array'
+import DArray from './Array/03-Add-Element-in-Array'
 
 const ary = new DArray()
+ary.addLast(44)
+ary.addLast(44)
+ary.addLast(44)
+ary.addLast(44)
+ary.addLast(44)
+
+ary.add(2, 55)
 
 console.log(ary.getSize())
+console.dir(ary)
